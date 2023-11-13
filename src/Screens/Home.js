1b9b03ed5bf54389c693cde0ea2ffc1a3612bd92
ReactwithHemanth <hemanth@sayonetech.com>
2023-11-13 18:17:12 +0530

    pushing changes in Home

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,10 +1,18 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {useAUth} from '../hooks/useAuth';
 
 const Home = () => {
+  const user = useAUth();
+  console.log(user.email);
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+      }}>
+      <Text style={{fontSize: 20, marginLeft: 30}}>Welcome {user.email}</Text>
     </View>
   );
 };
