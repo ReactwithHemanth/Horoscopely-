255fa24e6af11955150dd006f78f34514201a017
ReactwithHemanth <hemanth@sayonetech.com>
2023-11-13 21:54:36 +0530

    adding stylesheet

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,19 +1,22 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import {useAUth} from '../hooks/useAuth';
 
 const Home = () => {
   const user = useAUth();
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-      }}>
+    <View style={style.container}>
       <Text style={{fontSize: 20, marginLeft: 30}}>Welcome {user.email}</Text>
     </View>
   );
 };
 
 export default Home;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+});
