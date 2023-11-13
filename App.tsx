e6a8d0842ffbf5f65fd672d055d6e654e553ccae
admin <admin@admins-MacBook-Pro.local>
2023-11-13 10:28:21 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex:1,backgroundColor:'white'}}>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
